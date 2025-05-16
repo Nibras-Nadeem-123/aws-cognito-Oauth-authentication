7643dbb764f3dae4f6d467c62ab160e4f31aa725
Nibras-Nadeem-123 <nibrasnadeem621@gmail.com>
2025-05-16 23:36:47 +0500

    initial cummit

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from "react"
 import { useRouter } from "next/navigation"
-import { redirectToGoogleOAuth, signUp } from "@/lib/auth"
+import { redirectToGoogleOAuth, redirectToMicrosoftOAuth, signUp } from "@/lib/auth"
 // import { signInWithGoogle } from '@/lib/auth';
 
 export default function SignUpForm() {
@@ -64,13 +64,14 @@ export default function SignUpForm() {
             <button
               type="button"
               className="w-full py-3 px-4 border border-gray-300 rounded-md text-black hover:bg-gray-100 focus:outline-none flex items-center justify-center"
+              onClick={redirectToMicrosoftOAuth}
               disabled={isSubmitting}
             >
               {/* Apple Icon */}
               <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12.152 6.896c-...z" />
               </svg>
-              Sign up with Apple
+              Sign up with Microsoft
             </button>
 
             <button
